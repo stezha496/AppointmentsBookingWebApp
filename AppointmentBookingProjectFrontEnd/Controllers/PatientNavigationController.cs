c71a76341cdc41fd2d463f875708e73740e7d620
steven <stezha496@gmail.com>
2026-05-12 16:48:43 -0400

    Header and footer changes

--- AppointmentBookingProjectFrontEnd/Controllers/PatientNavigationController.cs
+++ AppointmentBookingProjectFrontEnd/Controllers/PatientNavigationController.cs
@@ -4,7 +4,25 @@ namespace AppointmentBookingProjectFrontEnd.Controllers;
 
 public class PatientNavigationController : Controller
 {
+    public IActionResult ChoosePhysician()
+    {
+        return View();
+    }
+
+    [HttpGet]
     public IActionResult PatientDashboard()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult ViewMyBookings()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult BookAppointment()
     {
         return View();
     }
